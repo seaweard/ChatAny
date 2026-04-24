707df49b0a94cde71c91ac5804350bb01da99f63
seaweard <xcz714583@gmail.com>
2026-04-25 01:17:34 +0800

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,7 @@ console.log("[Next] build with chunk: ", !disableChunk);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
